4f08c7ea0ab5cc37a6d6184494b16342a7fea0cc
Nirob Hasan <nirob427417@gmail.com>
2023-03-15 13:13:12 +0600

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import './normal.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <span>+ New Chat</span>
+        </div>
+      </aside>
+      <section className="chatbox">
+        <div className="chat-input-holder">
+          <textarea
+            rows='1'
+            className="chat-input-text-area"
+            placeholder=" "
+          ></textarea>
+        </div>
+      </section>
     </div>
   );
 }
